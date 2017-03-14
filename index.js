c1d1513c42e3b53c7b239f29e3e3147b9e0f13de
zeroclan8 <zeroclan8@gmail.com>
2017-03-14 11:03:06 +0800

    ip

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
     if(pathname == "/api/whoami"){
         // {"ipaddress":"107.178.195.199","language":"zh-CN","software":"Windows NT 10.0; WOW64"}
         var resultObj = { };
-        resultObj.ipaddress = request.connection.remoteAddress;
+        resultObj.ipaddress = request.connection.remoteAddress.substr(7);
         resultObj.language = request.headers["accept-language"].substr(0, request.headers["accept-language"].indexOf(","));
         resultObj.software = request.headers["user-agent"].substring(request.headers["user-agent"].indexOf("(") + 1, request.headers["user-agent"].indexOf(")"));
         response.writeHead(200, {"Content-Type": "text/plain"});
