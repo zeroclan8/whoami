19546db25cb7733c0e8de3828eb2dd0027be845e
zeroclan8 <zeroclan8@gmail.com>
2017-03-14 10:52:56 +0800

    首个版本

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var http = require("http");
+var url = require("url");
+http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("pathname:" + pathname);
+    if(pathname == "/api/whoami"){
+        // {"ipaddress":"107.178.195.199","language":"zh-CN","software":"Windows NT 10.0; WOW64"}
+        var resultObj = { };
+        resultObj.ipaddress = request.connection.remoteAddress;
+        resultObj.language = request.headers["accept-language"].substr(0, request.headers["accept-language"].indexOf(","));
+        resultObj.software = request.headers["user-agent"].substring(request.headers["user-agent"].indexOf("(") + 1, request.headers["user-agent"].indexOf(")"));
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(JSON.stringify(resultObj));
+        response.end();
+    }
+}).listen(8080);
